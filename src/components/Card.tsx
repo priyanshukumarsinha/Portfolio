5e6e2505f3439552ce12e60c0918ae9fdf66c97f
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2025-09-22 08:53:42 +0000

    feat: add liveLink property to projects and update Card component for improved link handling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,19 +7,28 @@ const Card = ({project}:any) => {
     if(project === undefined) return null;
     return (
         <RectangleBox className='md:max-w-[48%] sm:w-[48%] w-[100%]  h-[300px] justify-start items-start'>
+            <a href={project.link} target='_blank' rel='noreferrer' className='w-full'>
             <img 
             src={project.imageUrl}
             alt={project.title}
             className='w-full h-[180px] object-cover rounded-t-xs'
             />
+
+            </a>
             
             <div className="p-4">
                 <h3 className="text-white font-semibold block mb-1 truncate">
-                    {project.title}
+                    <a href={project.liveLink} className='hover:underline' target='_blank' rel='noreferrer'>
+                        {project.title}    
+                    </a> 
                 </h3>
                 <p className="text-white text-sm opacity-80 pt-2 line-clamp-2">
                     {project.description}
                 </p>
+            </div>
+
+            <div>
+                
             </div>
             
         </RectangleBox>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,6 +13,7 @@ interface Project {
   link: string;
   imageUrl: string;
   tags: string[];
+  liveLink: string;
 }
 
 
@@ -20,30 +21,34 @@ const projects: Project[] = [
     {
         title: "Vichaar",
         description: "Where JavaScript, React, and Firebase converge to craft an innovative blogging experience with streamlined deployment through GitHub.",
-        link: "0x.pks.com",
+        link: "https://github.com/priyanshukumarsinha/vichaar",
         imageUrl: vichaar,
-        tags: ["Node.js", "Express", "JavaScript"]
+        tags: ["Node.js", "Express", "JavaScript"],
+        liveLink: "https://www.vichaar.0xpks.site/"
     },
     {
         title: "HTTP Server",
         description: "HTTP follows a request-response model where the client sends a request, and the server responds.",
-        link: "0x.pks.com",
+        link: "https://github.com/priyanshukumarsinha/http-server",
         imageUrl: p2,
-        tags: ["React", "Tailwind CSS", "JavaScript"]
+        tags: ["React", "Tailwind CSS", "JavaScript"],
+        liveLink: "https://github.com/priyanshukumarsinha/http-server"
     },
     {
         title: "WebShield",
         description: "Comprehensive security analysis tool for web apps.",
-        link: "0x.pks.com",
+        link: "https://github.com/priyanshukumarsinha/webshield",
         imageUrl: p3,
-        tags: ["Node.js", "Socket.io", "JavaScript"]
+        tags: ["Node.js", "Socket.io", "JavaScript"],
+        liveLink: "https://github.com/priyanshukumarsinha/webshield"
     },
     {
         title: "Restraunt App",
         description: "A restaurant app built with React.js and Redux for seamless menu browsing, cart management, and food ordering.",
-        link: "0x.pks.com",
+        link: "https://github.com/priyanshukumarsinha/restarurantapp",
         imageUrl: p4,
-        tags: ["React", "Firebase", "JavaScript"]
+        tags: ["React", "Firebase", "JavaScript"],
+        liveLink: "https://github.com/priyanshukumarsinha/restarurantapp"
     }
 ]
 
@@ -58,7 +63,7 @@ const Projects = () => {
         <div className='flex flex-wrap justify-between gap-5'>
         {
             projects && projects.map((project, idx) =>
-                project ? <Card key={idx} project={project} /> : null
+                project ? <Card key={idx} project={project} />: null
             )
         }
         </div>
